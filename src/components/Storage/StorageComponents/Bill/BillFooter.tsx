a018fe605c14f8343efdf6283ecee81bbc81db61
aGoodMan999 <21521808@gm.uit.edu.vn>
2023-10-23 00:22:56 +0700

    Update UI BillFooter

--- src/components/Storage/StorageComponents/Bill/BillFooter.tsx
+++ src/components/Storage/StorageComponents/Bill/BillFooter.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useEffect} from 'react';
 import st from '../../style/bill-footer-style.module.css'
 import BillItemModel from '../../../../models/BillItemModel';
 export const BillFooter:React.FC<{checkOut: Function, billItem: BillItemModel[]}> = (props) => {
+    let total = 0;
+
+    props.billItem.forEach(billItem => {
+        total += billItem.amount??0;
+    });
+    
     const handleCheckOut = () => {
         props.checkOut();
     }
-    let total:number = 0;
-    useEffect(()=>{
-        props.billItem.forEach(billItem => {
-            total += billItem.amount??0;
-        });
-    }, [props.billItem])
+    
+
 
     return (
 
         <div className={st.sticky}>
             <p>{`Total: ${total}`}</p>
-            <button onClick={handleCheckOut}>Check out</button>
+            <button className={`${st.button} btn btn-success`} onClick={handleCheckOut}>Check out</button>
         </div>
     )
 }
