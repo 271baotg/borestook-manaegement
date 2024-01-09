101d8d7f5a301220c3187910b6dce6acc56191ce
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-01-09 15:06:45 +0700

    Update CustomerInfoModal: spent chỉ được chỉnh bởi admin, disable edit rank

--- src/components/Customer/components/CustomerInforModal.tsx
+++ src/components/Customer/components/CustomerInforModal.tsx
@@ -1,12 +1,13 @@
 import { Button, Form, Modal } from "react-bootstrap";
 import { CustomerModel } from "../../../models/CustomerModel";
 import { CustomerValidator } from "../../../utils/validators/CustomerValidator";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import AuthContext from "../../../auth/AuthProvider";
 
 const INIT_RANK: number = 0;
 const INIT_SPENT: number = 0;
 
-const validStyle = { fontFamily: 'monospace', fontSize: 13.5, color: 'green'};
+const validStyle = { fontFamily: 'monospace', fontSize: 13.5, color: 'green' };
 const inValidStyle = { fontFamily: 'monospace', fontSize: 13.5, color: 'red' };
 
 export const CustomerInforModal: React.FC<{
@@ -20,6 +21,7 @@ export const CustomerInforModal: React.FC<{
   title?: string;
 
 }> = (props) => {
+  const { auth } = useContext(AuthContext);
   const { validation = true } = props;
   const isShow = props.isShow;
   const customer = props.customer;
@@ -30,6 +32,9 @@ export const CustomerInforModal: React.FC<{
   };
 
   const handleOnInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!auth?.roles.includes('admin')) {
+      return;
+    }
     const { value, name } = e.target;
     props.setCustomer({ ...customer, [name]: value });
   };
@@ -67,9 +72,9 @@ export const CustomerInforModal: React.FC<{
               value={customer.fullName}
             ></Form.Control>
             {validation &&
-              <>
+              <div className="ms-2">
                 <p className="m-0" style={customerValidator.nameValidator.isLongEnough(props.customer.fullName ?? '') ? validStyle : inValidStyle}>(*) Name must contain more than 3 characters.</p>
-              </>
+              </div>
             }
           </Form.Group>
           <Form.Group>
@@ -81,20 +86,24 @@ export const CustomerInforModal: React.FC<{
               value={customer.phoneNumber}
             ></Form.Control>
             {validation &&
-              <>
+              <div className="ms-2">
                 <p className="m-0" style={customerValidator.phoneValidator.isLongEnough(props.customer.phoneNumber ?? '') ? validStyle : inValidStyle}>(*) Phonenumber length is exactly 10.</p>
                 <p className="m-0" style={customerValidator.phoneValidator.isRightFormat(props.customer.phoneNumber ?? '') ? validStyle : inValidStyle}>(*) Phonenumber have start with 0 (e.g. 0xxx...).</p>
-              </>
+              </div>
             }
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="mt-2">
             <Form.Label>Spent ($)</Form.Label>
+            <div className="d-inline-block ms-2">
+              <p style={{ fontFamily: 'monospace', fontSize: 13.5 }}>(Only admin can change this field)</p>
+            </div>
             <Form.Control
               name="spent"
               onChange={handleOnInputChanged}
               type="number"
               value={customer.spent}
             ></Form.Control>
+
           </Form.Group>
           <Form.Group>
             <Form.Label>Ranking</Form.Label>
@@ -103,6 +112,7 @@ export const CustomerInforModal: React.FC<{
               onChange={handleOnInputChanged}
               type="text"
               value={customer.ranking}
+              disabled
             ></Form.Control>
           </Form.Group>
         </Form>
