2557b751261dfa7ad48e0a5c29e27e6f18a6b60f
271baotg <21521862@gm.uit.edu.vn>
2024-05-29 10:32:57 +0700

    restructure folder

---
+++ backend/src/main/java/com/example/bookstore_backend/auth/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.example.bookstore_backend.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private List<String> roles;
+}
